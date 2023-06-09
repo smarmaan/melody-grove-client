74249e6273c6aa30058ccff8333cceeab0153f89
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-09 11:20:53 +0600

    login page design complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,21 +10,30 @@ const Login = () => {
       <div className="hero min-h-screen bg-[url('https://i.ibb.co/DfK7L2T/bg.jpg')] bg-cover">
         <div className="hero-overlay bg-opacity-30"></div>
 
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center md:w-1/2  lg:text-left shadow-xl drop-shadow-xl p-10 rounded-2xl text-white">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+        <div className="hero-content flex-col md:flex-row-reverse gap-48 md:gap-10">
+          <div className="text-center md:w-1/2 lg:text-left shadow-xl drop-shadow-xl  rounded-2xl text-white">
+            <>
+              <img
+                src="../../../public/music.gif"
+                className="w-full mt-20 md:-my-20 rounded-3xl absolute mx-auto"
+                alt=""
+              />
 
-            <Link to="/">
-              {" "}
-              <button className="btn glass text-white font-bold text-xl">
-                Home
-              </button>{" "}
-            </Link>
+              <div className="relative z-5 mt-24 md:mt-0 md:top-5 md:left-16">
+                <h1 className="text-4xl font-bold my-5">Login now!</h1>
+                <p className="">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Ipsum, exercitationem aliquid obcaecati iusto voluptatem
+                  aliquam fuga eaque qui optio amet!
+                </p>
+                <Link to="/">
+                  {" "}
+                  <button className="btn glass text-white font-bold md:text-xl my-5">
+                    Home
+                  </button>{" "}
+                </Link>
+              </div>
+            </>
           </div>
 
           <div className="card   bg-[url('https://images.unsplash.com/photo-1571330735066-03aaa9429d89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-cover drop-shadow-2xl md:w-1/2 max-w-sm shadow-2xl">
