9533d9227b84ca09dfca7e29c4c589316764f5f1
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-09 12:26:27 +0600

    login page design complete..

---
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,19 @@
+const SocialLogin = () => {
+  return (
+    <div>
+      <div className="divider "></div>
+
+      <div className="text-center mb-4">
+        <button className="btn btn-circle border-0">
+          <img
+            src="/public/google-logo-effect.gif"
+            className="rounded-full"
+            alt=""
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,18 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import SocialLogin from "../../Components/SocialLogin/SocialLogin";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div className="">
       <Helmet>
@@ -21,10 +32,10 @@ const Login = () => {
 
               <div className="relative z-5 mt-24 md:mt-0 md:top-5 md:left-16">
                 <h1 className="text-4xl font-bold my-5">Login now!</h1>
-                <p className="">
+                <p className="pr-16">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Ipsum, exercitationem aliquid obcaecati iusto voluptatem
-                  aliquam fuga eaque qui optio amet!
+                  aliquam fuga eaque q
                 </p>
                 <Link to="/">
                   {" "}
@@ -36,46 +47,61 @@ const Login = () => {
             </>
           </div>
 
-          <div className="card   bg-[url('https://images.unsplash.com/photo-1571330735066-03aaa9429d89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')] bg-cover drop-shadow-2xl md:w-1/2 max-w-sm shadow-2xl">
+          <div className="card   bg-[url('https://i.ibb.co/QNJMZCY/photo-1571330735066-03aaa9429d89-ixlib-rb-4-0.jpg')] bg-cover drop-shadow-2xl md:w-1/2 max-w-sm shadow-2xl">
             <div className="hero-overlay bg-opacity-10"></div>
 
-            <div className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                  type="text"
-                  placeholder="Email"
+                  type="email"
+                  placeholder="Enter your Email"
+                  name="email"
                   className="input input-bordered"
+                  required
                 />
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text ">Password</span>
+                  <span className="label-text">Password</span>
                 </label>
                 <input
                   type="text"
-                  placeholder="Password"
+                  placeholder="Enter your Password"
+                  name="password"
                   className="input input-bordered"
+                  required
                 />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-                <div className="mt-2">
-                  <Link to="/sign-up">
-                    <span className="p-2 bg-green-300 rounded-full ">
-                      sign-up
-                    </span>
-                  </Link>
-                </div>
               </div>
+              {/* 
+
+
+ */}
+
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
               </div>
-            </div>
+            </form>
+
+            <p className="text-center  md:text-xl">
+              <small className="flex-row items-center justify-center">
+                Are you new at Melody Grove?{" "}
+                <Link
+                  className="btn btn-primary btn-xs mt-5 w-24 mx-auto"
+                  to="/sign-up"
+                >
+                  Sign Up
+                </Link>
+              </small>
+            </p>
+
+            <SocialLogin></SocialLogin>
           </div>
         </div>
       </div>
