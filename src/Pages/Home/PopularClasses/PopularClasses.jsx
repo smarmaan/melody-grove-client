c464f9c206aaa625ffb698884aae439c0b2e993c
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-10 17:59:14 +0600

    show all show less button added

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -5,13 +5,24 @@ import PopularClassCard from "./PopularClassCard";
 
 const PopularClasses = () => {
   const [popular, setPopular] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll((prevShowAll) => !prevShowAll);
+  };
+
   useEffect(() => {
-    fetch("http://localhost:5000/popular-courses")
+    let url = "http://localhost:5000/popular-courses";
+    if (showAll) {
+      url += "?showAll=true";
+    }
+
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setPopular(data);
       });
-  }, []);
+  }, [showAll]);
 
   console.log(popular);
   return (
@@ -28,6 +39,13 @@ const PopularClasses = () => {
           <PopularClassCard key={info._id} info={info}></PopularClassCard>
         ))}
       </div>
+
+      <button
+        onClick={handleShowAll}
+        className="btn btn-primary text-center flex mx-auto my-24"
+      >
+        {showAll ? "Show Less" : "Show All"}
+      </button>
     </div>
   );
 };
