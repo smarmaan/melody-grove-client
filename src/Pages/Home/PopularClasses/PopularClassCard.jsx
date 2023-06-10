f5725614947548cca5003adf74cf2d31ddb6d264
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-10 18:52:02 +0600

    instructor json file added

--- src/Pages/Home/PopularClasses/PopularClassCard.jsx
+++ src/Pages/Home/PopularClasses/PopularClassCard.jsx
@@ -3,14 +3,14 @@ const PopularClassCard = ({ info }) => {
 
   return (
     <>
-      <div className=" card card-compact w-full h-full bg-base-100 shadow-xl">
+      <div className=" card card-compact w-full h-full shadow-2xl drop-shadow-2xl">
         <figure className="w-full h-full">
-          <img src={image} className="object-cover w-full h-full" alt={name} />
+          <img src={image} className="object-cover w-full h-full " alt={name} />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <h2 className="card-title">{students}</h2>
-          <h2 className="card-title">{rating}</h2>
+        <div className="card-body bg-[url('https://i.ibb.co/fFvCgts/image.png')] bg-cover rounded-ee-2xl rounded-es-2xl ">
+          <h2 className="card-title md:text-4xl">{name}</h2>
+          <h2 className="card-title">Number of Students: {students}</h2>
+          <h2 className="card-title">Ratings : {rating}</h2>
           <p>{description}</p>
           <p>
             {activities.map((activity, index) => (
