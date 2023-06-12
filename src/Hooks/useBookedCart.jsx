8feea10cfbc2af81e51569eac7d6d824efa3d1fa
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-12 10:22:44 +0600

    axios used to for the token validation

--- src/Hooks/useBookedCart.jsx
+++ src/Hooks/useBookedCart.jsx
@@ -1,18 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useBookedCart = () => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+
+  const [axiosSecure] = useAxiosSecure();
 
   const { refetch, data: bookedCart = [] } = useQuery({
     queryKey: ["booked-courses", user?.email],
+    enabled: !loading && !!user?.email,
+
     queryFn: async () => {
-      const res = await fetch(
-        `http://localhost:5000/booked-courses?email=${user?.email}`
-      );
-      const data = await res.json();
-      return data;
+      const res = await axiosSecure(`/booked-courses?email=${user?.email}`);
+
+      console.log("response from axios", res);
+
+      return res.data;
     },
   });
 
