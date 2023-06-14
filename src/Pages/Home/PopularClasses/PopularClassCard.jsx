0cc364013e0f40ea82f076375ff2268879d2e84d
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-14 09:09:23 +0600

    React-awesome-reveal used in popular class

--- src/Pages/Home/PopularClasses/PopularClassCard.jsx
+++ src/Pages/Home/PopularClasses/PopularClassCard.jsx
@@ -1,22 +1,33 @@
+import { Fade } from "react-awesome-reveal";
+
 const PopularClassCard = ({ info }) => {
   const { name, description, image, students, rating, activities } = info;
 
   return (
     <>
-      <div className=" card card-compact w-96  shadow-2xl drop-shadow-2xl">
+      <div className=" card card-compact w-96 shadow-2xl drop-shadow-2xl">
         <figure className="w-full h-full">
-          <img src={image} className="object-cover w-full h-96" alt={name} />
+          <img src={image} className="object-cover w-full h-80" alt={name} />
         </figure>
-        <div className="card-body bg-[#fc712b] bg-cover rounded-ee-2xl rounded-es-2xl ">
-          <h2 className="card-title md:text-4xl">{name}</h2>
-          <h2 className="card-title">Number of Students: {students}</h2>
-          <h2 className="card-title">Ratings : {rating}</h2>
-          <p>{description}</p>
-          <p>
+        <div className="card-body bg-[#fc712b] rounded-ee-2xl rounded-es-2xl ">
+          <Fade delay={1e3} cascade damping={1e-1}>
+            <h2 className="card-title md:text-4xl">{name}</h2>{" "}
+          </Fade>
+          <Fade delay={1e3} cascade damping={1e-1} className="card-title">
+            <h2>Number of Students: {students}</h2>
+          </Fade>
+          <Fade delay={1e3} cascade damping={1e-1} className="card-title">
+            <h2>Ratings : {rating}</h2>
+          </Fade>
+
+          <Fade delay={1e3} cascade damping={1e-1}>
+            <p>{description}</p>
+          </Fade>
+          <Fade delay={1e3} cascade damping={1e-1}>
             {activities.map((activity, index) => (
               <li key={index}>{activity}</li>
             ))}
-          </p>
+          </Fade>
         </div>
       </div>
     </>
