28b48e95250e2b0ddebb7d8bbc1aedd310e30671
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-14 16:23:08 +0600

    links added in extra text file..

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar fixed z-10 bg-opacity-50 bg-orange-800 font-Cambria font-extrabold">
+    <div className="navbar fixed z-10 bg-opacity-80 bg-cyan-800 font-Cambria font-extrabold">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
