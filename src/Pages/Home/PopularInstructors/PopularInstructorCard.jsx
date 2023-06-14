62a3ece0dc1df155e93ad8d3c298de9e25918a00
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-14 09:13:51 +0600

    react-awesome-reveal used in instructor section

--- src/Pages/Home/PopularInstructors/PopularInstructorCard.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructorCard.jsx
@@ -1,3 +1,5 @@
+import { Fade } from "react-awesome-reveal";
+
 const PopularInstructorCard = ({ info }) => {
   const { name, email, image, classes } = info;
 
@@ -7,16 +9,24 @@ const PopularInstructorCard = ({ info }) => {
         <img src={image} className="object-cover w-full h-96" alt={name} />
       </>
       <div className="card-body  bg-opacity-30 opacity-0 hover:opacity-100 flex-col justify-center items-center bg-[#ffe36b] bg-cover rounded-2xl absolute inset-0">
-        <h2 className="card-title md:text-3xl">{name}</h2>
-        <h2 className="card-title text-[17px] md:text-lg">Email: {email}</h2>
-        <h2 className="card-title md:text-lg">
-          Instructor for{" "}
-          {classes.map((classes, index) => (
-            <div className="flex-col gap-10" key={index}>
-              <h2 className="card-title md:text-lg">{classes.name} </h2>
-            </div>
-          ))}
-        </h2>
+        <Fade delay={1e3} cascade damping={1e-1}>
+          <h2 className="card-title md:text-3xl">{name}</h2>
+        </Fade>
+
+        <Fade delay={1e3} cascade damping={1e-1}>
+          <h2 className="card-title text-[17px] md:text-lg">Email: {email}</h2>
+        </Fade>
+
+        <Fade delay={1e3} cascade damping={1e-1}>
+          <h2 className="card-title md:text-lg">
+            Instructor for{" "}
+            {classes.map((classes, index) => (
+              <div className="flex-col gap-10" key={index}>
+                <h2 className="card-title md:text-lg">{classes.name} </h2>
+              </div>
+            ))}
+          </h2>
+        </Fade>
       </div>
     </div>
   );
